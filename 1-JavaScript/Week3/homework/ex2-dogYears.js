be7330b36061a9a017e4adadfc4e852213e40c6e
Veronika <veroniqe@rambler.ru>
2021-02-20 21:29:35 +0100

    changes in week3-ex.2

--- 1-JavaScript/Week3/homework/ex2-dogYears.js
+++ 1-JavaScript/Week3/homework/ex2-dogYears.js
@@ -15,9 +15,8 @@ calculate it!
 -----------------------------------------------------------------------------*/
 
 function calculateDogAge(age) {
-  // TODO complete this function
-  age = age * 7;
-  return `Your doggie is ${age} years old in dog years!`;
+  const dogAge = age * 7;
+  return `Your doggie is ${dogAge} years old in dog years!`;
 }
 
 console.log(calculateDogAge(1)); // -> "Your doggie is 7 years old in dog years!"
