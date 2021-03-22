c6fef559e5b2bb67192ede983bd49fd701ffba9a
Veronika <veroniqe@rambler.ru>
2021-03-22 14:15:07 +0100

    ex5 is done

---
+++ 3-UsingAPIs/Week1/homework/ex5-rollDiceChain.js
@@ -0,0 +1,31 @@
+// Challenge: throw five dices in sequence
+const rollDice = require('../../helpers/pokerDiceRoller');
+function rollTheDices() {
+  const results = [];
+
+  rollDice(1)
+    .then((value) => {
+      results.push(value);
+      return rollDice(2);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDice(3);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDice(4);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDice(5);
+    })
+    .then((value) => {
+      results.push(value);
+      console.log('Resolved!', results);
+    })
+    .catch((error) => console.log('Rejected!', error.message));
+}
+rollTheDices();
+// ! Do not change or remove the code below
+module.exports = rollTheDices;
