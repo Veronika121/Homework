0a4738a10274eef85a4365570e8732db9df428eb
Veronika <veroniqe@rambler.ru>
2021-03-22 19:13:01 +0100

    ex5 is changed

--- 3-UsingAPIs/Week1/homework/ex5-rollDiceChain.js
+++ 3-UsingAPIs/Week1/homework/ex5-rollDiceChain.js
@@ -3,29 +3,23 @@ const rollDice = require('../../helpers/pokerDiceRoller');
 function rollTheDices() {
   const results = [];
 
+  const pushAndRoll = (dice) => (value) => {
+    results.push(value);
+    return rollDice(dice);
+  };
+
   rollDice(1)
-    .then((value) => {
-      results.push(value);
-      return rollDice(2);
-    })
-    .then((value) => {
-      results.push(value);
-      return rollDice(3);
-    })
-    .then((value) => {
-      results.push(value);
-      return rollDice(4);
-    })
-    .then((value) => {
-      results.push(value);
-      return rollDice(5);
-    })
+    .then(pushAndRoll(2))
+    .then(pushAndRoll(3))
+    .then(pushAndRoll(4))
+    .then(pushAndRoll(5))
     .then((value) => {
       results.push(value);
       console.log('Resolved!', results);
     })
     .catch((error) => console.log('Rejected!', error.message));
 }
+
 rollTheDices();
 // ! Do not change or remove the code below
 module.exports = rollTheDices;
